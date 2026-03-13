5f2e7b0ff406d0e1133ada1d2686f83a6e21890e
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-13 23:05:54 +0300

    Create VehicleModulesPlugin.cs

---
+++ VehicleModulesPlugin.cs
@@ -0,0 +1,73 @@
+using Rocket.Core.Plugins;
+using Rocket.API.Collections;
+using SDG.Unturned;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace VehicleModulesSystem
+{
+    public class VehicleModulesPlugin : RocketPlugin<VehicleModulesConfig>
+    {
+        public static VehicleModulesPlugin Instance;
+        public Dictionary<uint, Dictionary<string, float>> SavedVehicleData = new Dictionary<uint, Dictionary<string, float>>();
+        private string dataPath;
+        private float lastSaveTime;
+        public bool IsDirty = false; // Флаг: нужно ли сохранять данные
+
+        protected override void Load()
+        {
+            Instance = this;
+            dataPath = Path.Combine(Directory, "VehicleData.json");
+            LoadData();
+
+            VehicleManager.onVehicleSpawned += OnVehicleSpawned;
+            foreach (var v in VehicleManager.vehicles) AddTracker(v);
+        }
+
+        void Update()
+        {
+            // Автосохранение только если данные изменились
+            if (IsDirty && Time.time - lastSaveTime > Configuration.Instance.SaveInterval)
+            {
+                SaveData();
+            }
+        }
+
+        protected override void Unload()
+        {
+            SaveData();
+            VehicleManager.onVehicleSpawned -= OnVehicleSpawned;
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+            { "Status_Perfect", "Штатно" },
+            { "Status_Damaged", "Поврежден" },
+            { "Status_Critical", "Критический износ" },
+            { "Status_Destroyed", "РАЗРУШЕН" },
+            { "Alert_ChainReaction", "[!] Топливо вспыхнуло от перегрева!" }
+        };
+
+        private void OnVehicleSpawned(InteractableVehicle v) => AddTracker(v);
+        private void AddTracker(InteractableVehicle v)
+        {
+            if (v != null && v.gameObject.GetComponent<VehicleTracker>() == null)
+                v.gameObject.AddComponent<VehicleTracker>();
+        }
+
+        public void SaveData() 
+        { 
+            File.WriteAllText(dataPath, JsonConvert.SerializeObject(SavedVehicleData)); 
+            lastSaveTime = Time.time;
+            IsDirty = false;
+        }
+
+        private void LoadData() 
+        { 
+            if (File.Exists(dataPath)) 
+                SavedVehicleData = JsonConvert.DeserializeObject<Dictionary<uint, Dictionary<string, float>>>(File.ReadAllText(dataPath)); 
+        }
+    }
+}
