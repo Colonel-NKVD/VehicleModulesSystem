b8da8936f6f559cacd6c8ef85054cf8f335e943c
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-14 00:09:17 +0300

    Update VehicleModulesPlugin.cs

--- VehicleModulesPlugin.cs
+++ VehicleModulesPlugin.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Rocket.API.Collections;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
+using Rocket.Unturned.Player;
 using SDG.Unturned;
 using UnityEngine;
 
@@ -11,7 +12,7 @@ namespace VehicleModulesSystem
     {
         public static VehicleModulesPlugin Instance;
         
-        // Поля, без которых VehicleTracker.cs выдаст ошибку при компиляции
+        // Поля для работы VehicleTracker.cs
         public Dictionary<uint, Dictionary<string, float>> SavedVehicleData = new Dictionary<uint, Dictionary<string, float>>();
         public bool IsDirty = false;
 
@@ -19,24 +20,28 @@ namespace VehicleModulesSystem
         {
             Instance = this;
 
-            // Используем наиболее вероятное имя события для твоей версии
-            VehicleManager.onVehicleSpawned += OnVehicleSpawned;
+            // КРЕАТИВНЫЙ ОБХОД: Вместо спавна машин следим за игроками.
+            // При входе любого игрока проверяем все машины на наличие трекера.
+            U.Events.OnPlayerConnected += OnPlayerConnected;
         }
 
         protected override void Unload()
         {
-            VehicleManager.onVehicleSpawned -= OnVehicleSpawned;
+            U.Events.OnPlayerConnected -= OnPlayerConnected;
         }
 
-        private void OnVehicleSpawned(InteractableVehicle vehicle)
+        private void OnPlayerConnected(UnturnedPlayer player)
         {
-            if (vehicle != null && vehicle.gameObject.GetComponent<VehicleTracker>() == null)
+            // Сканируем все машины в мире и вешаем наш компонент
+            foreach (InteractableVehicle vehicle in VehicleManager.vehicles)
             {
-                vehicle.gameObject.AddComponent<VehicleTracker>();
+                if (vehicle != null && vehicle.gameObject.GetComponent<VehicleTracker>() == null)
+                {
+                    vehicle.gameObject.AddComponent<VehicleTracker>();
+                }
             }
         }
 
-        // Переводы для вывода статуса в CommandVStat
         public override TranslationList DefaultTranslations => new TranslationList
         {
             { "Status_Perfect", "Исправен" },
