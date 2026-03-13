5602c1f926f5121a794bd7e46ccb92c6ae12b42b
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-13 23:19:29 +0300

    Update CommandVStat.cs

--- CommandVStat.cs
+++ CommandVStat.cs
@@ -1,17 +1,38 @@
-public void Execute(IRocketPlayer caller, string[] command)
+using System;
+using System.Collections.Generic;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+using UnityEngine;
+
+namespace VehicleModulesSystem
 {
-    UnturnedPlayer player = (UnturnedPlayer)caller;
-    var tracker = player.CurrentVehicle?.gameObject.GetComponent<VehicleTracker>();
-
-    if (tracker == null) return;
-
-    UnturnedChat.Say(caller, "--- ТЕХНИЧЕСКИЙ ФОРМУЛЯР ---", Color.yellow);
-    foreach (TankModule mod in Enum.GetValues(typeof(TankModule)))
+    public class CommandVStat : ICommand
     {
-        string statusText = tracker.GetModuleStatus(mod);
-        int percent = (int)tracker.ModuleHP[mod];
-        
-        Color c = percent > 70 ? Color.green : (percent > 30 ? Color.yellow : Color.red);
-        UnturnedChat.Say(caller, $"{mod}: {statusText} ({percent}%)", c);
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "vstat";
+        public string Help => "Проверить модули танка";
+        public string Syntax => "/vstat";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string> { "tank.status" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            var tracker = player.CurrentVehicle?.gameObject.GetComponent<VehicleTracker>();
+
+            if (tracker == null) return;
+
+            UnturnedChat.Say(caller, "--- ТЕХНИЧЕСКИЙ ФОРМУЛЯР ---", Color.yellow);
+            foreach (TankModule mod in Enum.GetValues(typeof(TankModule)))
+            {
+                string statusText = tracker.GetModuleStatus(mod);
+                int percent = (int)tracker.ModuleHP[mod];
+                
+                Color c = percent > 70 ? Color.green : (percent > 30 ? Color.yellow : Color.red);
+                UnturnedChat.Say(caller, $"{mod}: {statusText} ({percent}%)", c);
+            }
+        }
     }
 }
