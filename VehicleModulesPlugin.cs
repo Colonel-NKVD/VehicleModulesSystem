faf40cb3fead9f2993c30d15b8b41bce0d2bd37f
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-13 23:44:22 +0300

    Update VehicleModulesPlugin.cs

--- VehicleModulesPlugin.cs
+++ VehicleModulesPlugin.cs
@@ -1,73 +1,34 @@
+using Rocket.Unturned;
 using Rocket.Core.Plugins;
-using Rocket.API.Collections;
 using SDG.Unturned;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
 using UnityEngine;
 
 namespace VehicleModulesSystem
 {
-    public class VehicleModulesPlugin : RocketPlugin<VehicleModulesConfig>
+    public class VehicleModulesPlugin : RocketPlugin<VehicleModulesConfiguration>
     {
         public static VehicleModulesPlugin Instance;
-        public Dictionary<uint, Dictionary<string, float>> SavedVehicleData = new Dictionary<uint, Dictionary<string, float>>();
-        private string dataPath;
-        private float lastSaveTime;
-        public bool IsDirty = false; // Флаг: нужно ли сохранять данные
 
         protected override void Load()
         {
             Instance = this;
-            dataPath = Path.Combine(Directory, "VehicleData.json");
-            LoadData();
 
-            VehicleManager.onVehicleAdded += OnVehicleSpawned;
-            foreach (var v in VehicleManager.vehicles) AddTracker(v);
-        }
-
-        void Update()
-        {
-            // Автосохранение только если данные изменились
-            if (IsDirty && Time.time - lastSaveTime > Configuration.Instance.SaveInterval)
-            {
-                SaveData();
-            }
+            // Используем событие добавления в регион
+            VehicleManager.onVehicleRegionAdded += OnVehicleSpawned;
         }
 
         protected override void Unload()
         {
-            SaveData();
-            VehicleManager.onVehicleAdded -= OnVehicleSpawned;
+            VehicleManager.onVehicleRegionAdded -= OnVehicleSpawned;
         }
 
-        public override TranslationList DefaultTranslations => new TranslationList
+        // Обновленная сигнатура метода: принимает регион и транспорт
+        private void OnVehicleSpawned(VehicleRegion region, InteractableVehicle vehicle)
         {
-            { "Status_Perfect", "Штатно" },
-            { "Status_Damaged", "Поврежден" },
-            { "Status_Critical", "Критический износ" },
-            { "Status_Destroyed", "РАЗРУШЕН" },
-            { "Alert_ChainReaction", "[!] Топливо вспыхнуло от перегрева!" }
-        };
-
-        private void OnVehicleSpawned(InteractableVehicle v) => AddTracker(v);
-        private void AddTracker(InteractableVehicle v)
-        {
-            if (v != null && v.gameObject.GetComponent<VehicleTracker>() == null)
-                v.gameObject.AddComponent<VehicleTracker>();
-        }
-
-        public void SaveData() 
-        { 
-            File.WriteAllText(dataPath, JsonConvert.SerializeObject(SavedVehicleData)); 
-            lastSaveTime = Time.time;
-            IsDirty = false;
-        }
-
-        private void LoadData() 
-        { 
-            if (File.Exists(dataPath)) 
-                SavedVehicleData = JsonConvert.DeserializeObject<Dictionary<uint, Dictionary<string, float>>>(File.ReadAllText(dataPath)); 
+            if (vehicle != null && vehicle.gameObject.GetComponent<VehicleTracker>() == null)
+            {
+                vehicle.gameObject.AddComponent<VehicleTracker>();
+            }
         }
     }
 }
