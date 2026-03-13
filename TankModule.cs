d94bac7f038f6d05fc233ad759229bc8016ffefc
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-13 23:27:47 +0300

    Create TankModule.cs

---
+++ TankModule.cs
@@ -0,0 +1,10 @@
+namespace VehicleModulesSystem
+{
+    public enum TankModule
+    {
+        Engine,        // Двигатель
+        Transmission,  // Трансмиссия
+        FuelLeak,      // Топливная система
+        Fire           // Противопожарная система
+    }
+}
