08bf216c7de46f10e0d4671e86eca4c4884bfc10
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-15 01:00:13 +0300

    Update and rename VehicleModulesConfiguration.cs to VehicleModulesConfig.cs

---
+++ VehicleModulesConfig.cs
@@ -0,0 +1,34 @@
+using Rocket.API;
+using System.Collections.Generic;
+
+namespace VehicleModulesSystem
+{
+    public class VehicleModulesConfig : IRocketPluginConfiguration
+    {
+        public List<ushort> TargetedVehicleIds;
+        public ushort RepairStationBarricadeId;
+        public float RepairStationRadius;
+
+        // Шансы поломок (от 0.0 до 1.0)
+        public float ChanceFuelLeak;
+        public float ChanceTransmission;
+        public float ChanceGunBroken;
+        public float ChanceFire;
+        public float ChanceSmoke;
+        public float ChanceStun;
+
+        public void Defaults()
+        {
+            TargetedVehicleIds = new List<ushort> { 120, 121, 137 };
+            RepairStationBarricadeId = 55000;
+            RepairStationRadius = 20f;
+
+            ChanceFuelLeak = 0.05f;
+            ChanceTransmission = 0.05f;
+            ChanceGunBroken = 0.05f;
+            ChanceFire = 0.03f;
+            ChanceSmoke = 0.07f;
+            ChanceStun = 0.05f;
+        }
+    }
+}
