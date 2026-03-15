2da82fd206706b86bd11e790d22e523734814069
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-15 12:09:02 +0300

    Update VehicleModulesConfig.cs

--- VehicleModulesConfig.cs
+++ VehicleModulesConfig.cs
@@ -5,13 +5,25 @@ namespace VehicleModulesSystem
 {
     public class VehicleModulesConfig : IRocketPluginConfiguration
     {
-        // Базовый список ID, которые плагин будет игнорировать или обрабатывать
-        // В будущем сюда добавим конкретные шансы для разных типов урона
         public List<ushort> TargetedVehicleIds;
+        
+        // Шансы (0.0 - 1.0)
+        public float ChanceFuelLeak;
+        public float ChanceTransmission;
+        public float ChanceGunBroken;
+        public float ChanceFire;
+        public float ChanceSmoke;
+        public float ChanceStun;
 
         public void LoadDefaults()
         {
             TargetedVehicleIds = new List<ushort> { 120, 121, 137 };
+            ChanceFuelLeak = 0.15f;
+            ChanceTransmission = 0.10f;
+            ChanceGunBroken = 0.12f;
+            ChanceFire = 0.05f;
+            ChanceSmoke = 0.20f;
+            ChanceStun = 0.10f;
         }
     }
 }
