29cf2c03f13deb4619da55289c69aba31f4636e1
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-15 11:49:57 +0300

    Update VehicleModulesConfig.cs

--- VehicleModulesConfig.cs
+++ VehicleModulesConfig.cs
@@ -5,14 +5,15 @@ namespace VehicleModulesSystem
 {
     public class VehicleModulesConfig : IRocketPluginConfiguration
     {
-        public List<ushort> TargetedVehicleIds; // Список ID танков/броневиков
-        public float ChanceFuelLeak;            // Шанс пробития бака (0.0 - 1.0)
-        public float ChanceTransmission;        // Шанс поломки коробки
-        public float ChanceFire;                // Шанс пожара
+        public List<ushort> TargetedVehicleIds; 
+        public float ChanceFuelLeak;            
+        public float ChanceTransmission;        
+        public float ChanceFire;                
         
-        public void Defaults()
+        // Исправлено: метод должен называться LoadDefaults
+        public void LoadDefaults()
         {
-            TargetedVehicleIds = new List<ushort> { 120, 121, 137 }; // Примеры ID
+            TargetedVehicleIds = new List<ushort> { 120, 121, 137 }; 
             ChanceFuelLeak = 0.15f;
             ChanceTransmission = 0.10f;
             ChanceFire = 0.05f;
