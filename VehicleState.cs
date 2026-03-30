3b58e1ec70014adec3fe408bb012b6c36b7580ba
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-30 19:27:41 +0300

    Create VehicleState.cs

---
+++ VehicleState.cs
@@ -0,0 +1,15 @@
+namespace VehicleModulesSystem
+{
+    public class VehicleState
+    {
+        public ushort LastHealth;
+        public uint InstanceID;
+        public bool IsFuelTankBroken;
+        public bool IsTransmissionBroken;
+        public bool IsGunBroken;
+        public bool IsOnFire;
+        public bool IsSmoking;
+        public bool IsStunned;
+        public bool IsRepairing;
+    }
+}
